640b42dc2ecea34b3c1c10b1f8145dcd6add7fe0
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-10 01:40:15 +0530

    fixed the script

--- clean_env.sh
+++ clean_env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 dots=$(ls -d */ | grep -v ^_)
+rm -rf ~/.zshrc ~/.zshrc-personal ~/.config/bspwm ~/.config/Alacritty ~/.config/lf ~/.config/polybar ~/.config/zathura ~/.config/ ~/.config/nvim
 for dot in $dots
 do
 	stow -D $dot
