26675963dd5d78258919a4daa397f270f4501e30
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-12 16:07:02 +0530

    bug fix

--- installscripts/newinstanceinstall.sh
+++ installscripts/newinstanceinstall.sh
@@ -5,7 +5,7 @@
 echo "Updating your system!!!"
 yay -Syu --noconfirm
 yay -Rns conky --noconfirm
-cd ~/.dotfiles/misc/;bash install_progs.sh
+cd ~/.dotfiles/misc/;bash install_prog.sh
 echo "Installed dependencies"
 cd ~/.dotfiles/nord-dmenu5.0/; sudo make install
 echo "Built dmenu from source"
