7fd7f189837441ee9ecd5d62566e3c1bb41421b5
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-13 22:11:58 +0530

    Warning the scripts are broken!!!

--- installscripts/newinstanceinstall.sh
+++ installscripts/newinstanceinstall.sh
@@ -8,8 +8,8 @@ cd ~/.dotfiles/installscripts/;bash installdependencies.sh
 echo "Installed dependencies"
 cd ~/.dotfiles/nord-dmenu5.0/; sudo make install
 echo "Built dmenu from source"
-cd ~/.dotfiles/.config/;/usr/bin/ls -d */ | xargs -I {} -n 1 rm -rf ~/.config/{}
-cd ~/.dotfiles/.home/;/usr/bin/ls -d */ |sed 's/\/$//'| xargs -I {} -n 1 rm -rf ~/.{}
+cd ~/.dotfiles/.config/;/usr/bin/ls -d */ | sed 's/\/$//' | xargs -I {} -n 1 rm -rf ~/.config/{}
+cd ~/.dotfiles/.home/;/usr/bin/ls -d */ | sed 's/\/$//' | xargs -I {} -n 1 rm -rf ~/.{}
 echo "Removed any config files which may cause conflicts."
 cd ~/.dotfiles/.config/;bash install.sh
 echo "Symlinked all .config files"
