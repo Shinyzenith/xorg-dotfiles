caa6cf203fbd25c78932b50bbe3e721f3ea27db0
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-14 19:05:04 +0530

    Bug fix

--- installscripts/installdependencies.sh
+++ installscripts/installdependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yay
-echo $(cat ./standard.txt)|xargs -I {} pacman -S {} --noconfirm
+echo $(cat ./standard.txt)|xargs -I {} sudo pacman -S {} --noconfirm
 echo $(cat ./aur.txt)|xargs -I {} yay -S {} --noconfirm
