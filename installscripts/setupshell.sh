59f5c2e02f93ed7be4d7946330de7007653e4e8f
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-14 20:49:15 +0530

    Small changes

--- installscripts/setupshell.sh
+++ installscripts/setupshell.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installed oh my zsh"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo "Installed starship prompt"
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
