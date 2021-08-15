6a841dd7217aa200d4e4458c59503560a3934fb6
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-15 22:40:09 +0530

    bug fix

--- installscripts/installdependencies.sh
+++ installscripts/installdependencies.sh
@@ -1,4 +1,4 @@
-sudo pacman -S intel-ucode bspwm sxhkd zsh xorg lxapperance xorg-xinit maim sxiv blueberry neovim bashtop pulsemixer pcmanfm zathura zathura-pdf-mupdf zathura-ps otf-font-awesome otf-font-awesome-5-free ttf-font-awesome obs-studio stow alacritty exa nitrogen brightnessctl pcmanfm git xorg-xmodmap xorg-xsetroot networkmanager xfce4-notifyd xfce4-power-manager polkit polkit-gnome neofetch numlockx
-yay -S lf polybar brave-bin spotify gotop cherry-font-git ttf-mac-fonts nerd-fonts-jetbrains-mono discord-canary nordic-darker-theme picom-ibhagwan-git freetube-bin ttf-font-logos 
+sudo pacman -S intel-ucode bspwm sxhkd zsh xorg lxappearance xorg-xinit maim sxiv blueberry neovim bashtop pulsemixer pcmanfm zathura zathura-pdf-mupdf zathura-ps otf-font-awesome ttf-font-awesome obs-studio stow alacritty exa nitrogen brightnessctl pcmanfm git xorg-xmodmap xorg-xsetroot networkmanager xfce4-notifyd xfce4-power-manager polkit polkit-gnome neofetch numlockx
+yay -S lf polybar brave-bin spotify gotop cherry-font-git ttf-mac-fonts nerd-fonts-jetbrains-mono discord-canary nordic-darker-theme picom-ibhagwan-git freetube-bin ttf-font-logos otf-font-awesome-5-free 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Installed oh my zsh"
