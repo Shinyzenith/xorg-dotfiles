b5c0b9b42bf22b0013a1ddc2ef55f2beea55250c
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-16 01:27:27 +0530

    edited the scripts

--- installscripts/installdependencies.sh
+++ installscripts/installdependencies.sh
@@ -1,9 +1,28 @@
-sudo pacman -S intel-ucode python3 python-pip nodejs npm bspwm sxhkd zsh xorg lxappearance xorg-xinit maim sxiv blueberry neovim bashtop pulsemixer pcmanfm zathura zathura-pdf-mupdf zathura-ps otf-font-awesome ttf-font-awesome obs-studio stow alacritty exa nitrogen brightnessctl pcmanfm git xorg-xmodmap xorg-xsetroot networkmanager xfce4-notifyd xfce4-power-manager polkit polkit-gnome neofetch numlockx
-yay -S lf polybar brave-bin paper-icon-theme spotify gotop cherry-font-git ttf-mac-fonts nerd-fonts-jetbrains-mono discord-canary nordic-darker-theme picom-ibhagwan-git freetube-bin ttf-font-logos otf-font-awesome-5-free bat adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-sans-fonts awesome-terminal-fonts cherry-font-git gsfonts nerd-fonts-jetbrains-mono nerd-fonts-source-code-pro noto-fonts otf-font-awesome terminus-font ttf-font-awesome ttf-mac-fonts ttf-ubuntu-font-family xorg-mkfontscale
+sudo pacman -S xorg xorg-init xorg-modmap xorg-xsetroot
+
+sudo pacman -S intel-ucode network-manager-applet python3 python-pip nodejs npm bspwm sxhkd zsh lxappearance maim sxiv blueberry neovim bashtop pulsemixer pcmanfm zathura zathura-pdf-mupdf zathura-ps otf-font-awesome ttf-font-awesome obs-studio stow alacritty exa nitrogen brightnessctl pcmanfm git networkmanager xfce4-notifyd xfce4-power-manager polkit polkit-gnome neofetch numlockx
+
+yay -S lf bitwarden-bin polybar brave-bin paper-icon-theme spotify gotop cherry-font-git ttf-mac-fonts nerd-fonts-jetbrains-mono discord-canary nordic-darker-theme picom-ibhagwan-git freetube-bin ttf-font-logos otf-font-awesome-5-free bat adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-sans-fonts awesome-terminal-fonts cherry-font-git gsfonts nerd-fonts-jetbrains-mono nerd-fonts-source-code-pro noto-fonts otf-font-awesome terminus-font ttf-font-awesome ttf-mac-fonts ttf-ubuntu-font-family
+
 sudo python3 -m pip install neovim
 sudo npm install neovim --global
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installed oh my zsh"
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Installed vim plug"
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo "Copied xinitrc to home directory, configure it to your needs"
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installed oh my zsh"
+
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Added zsh autocomplete"
+
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Added zsh syntax highlighting"
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+echo "Installed starship prompt"
--- installscripts/setupshell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-echo "Installed starship prompt"
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "Added zsh autocomplete"
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "Added zsh syntax highlighting"
