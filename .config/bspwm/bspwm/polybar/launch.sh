05a385123cfd3abedb14510a3c8bd5651379104e
AakashSharma7269 <aakashsensharma@gmail.com>
2021-08-18 15:14:09 +0530

    Some changes

--- .config/bspwm/bspwm/polybar/launch.sh
+++ .config/bspwm/bspwm/polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -c ~/.config/polybar/config -rq bar &
+polybar -c ~/.config/bspwm/polybar/config -rq bar &
 
 echo "Polybar launched..."
