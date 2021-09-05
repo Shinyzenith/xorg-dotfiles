3e49b9c260af508a069e43dc4ae786a30965caa8
AakashSharma7269 <aakashsensharma@gmail.com>
2021-09-05 20:51:53 +0530

    Minor changes, nothing serious

--- 600-misc-commands.sh
+++ 600-misc-commands.sh
@@ -8,7 +8,6 @@ mkdir -p ~/Videos/recordings
 mkdir -p ~/Pictures/screenshots
 touch -p ~/.config/neofetch/neofetchcache
 mkdir -p ~/.vim/undodir
-sudo chown $USER /usr/share/backgrounds/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
@@ -16,6 +15,7 @@ sudo mkdir -p /usr/share/zsh/plugins
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting
 mkdir -p ~/.icons/default
+sudo chown $USER /usr/share/backgrounds/
 touch ~/.icons/default/index.theme
 echo "[icon theme]" >> ~/.icons/default/index.theme
 echo "Inherits macOSBigSur" >> ~/.icons/default/index.theme
