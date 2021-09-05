5d127006754a4b691070b2a8e82949f11bd735ca
AakashSharma7269 <aakashsensharma@gmail.com>
2021-09-05 18:48:15 +0530

    Script update

--- .config/bspwm/bspwm/scripts/enableclickontap.sh
+++ .config/bspwm/bspwm/scripts/enableclickontap.sh
@@ -1,7 +1,4 @@
 #1/bin/sh
-export DISPLAY=:0
-declare -i ID
 ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
-declare -i PROP
 PROP=`xinput list-props $ID | grep 'Tapping Enabled' -m 1 | awk '{print $4}' |  grep -o -E '[0-9]+'`
 xinput set-prop $ID $PROP 1
