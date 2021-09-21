0c9dc8339ecdb3c88a5090cba3c1ec820bfb0fa8
AakashSharma7269 <aakashsensharma@gmail.com>
2021-09-21 18:43:01 +0530

    Dots update

--- 000-install-setup.sh
+++ 000-install-setup.sh
@@ -3,6 +3,6 @@ cd ~/.dotfiles/
 ./100-install-packages.sh
 cd ~/.dotfiles/
 ./200-install-configs.sh
-sudo pacman -Rns --noconfirm firefox iwd dhcpcd # i don't need these anymore
+sudo pacman -Rns --noconfirm nano firefox iwd dhcpcd # i don't need these anymore
 yay -c
 # for when i fuck up dual boot https://dev.to/nicolasbeauvais/repair-windows-efi-partition-after-a-linux-installation-157o
