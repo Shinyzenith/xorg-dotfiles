bff06933aef023068953762652c3e37094f103f8
AakashSharma7269 <aakashsensharma@gmail.com>
2021-10-03 22:01:52 +0530

    minor changes

--- 000-install-setup.sh
+++ 000-install-setup.sh
@@ -7,4 +7,7 @@ sudo pacman -Rns --noconfirm nano firefox iwd dhcpcd # i don't need these anymor
 yay -c
 cd ~/.bin
 wget https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl -O fm6000
+mkdir -p ~/.bin
+cp ~/.config/bspwm/scripts/piratesong ~/.bin/
+cp ~/.config/bspwm/scripts/killprocess ~/.bin/
 # for when i fuck up dual boot https://dev.to/nicolasbeauvais/repair-windows-efi-partition-after-a-linux-installation-157o
