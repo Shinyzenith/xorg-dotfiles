ffd20f4d610e0b36efc8d938377c82dc48c98f62
Shinyzenith <aakashsensharma@gmail.com>
2021-10-28 10:02:19 +0530

    script update

--- wayland/setup.sh
+++ wayland/setup.sh
@@ -1,11 +1,15 @@
-sudo pacman -S stow --needed --noconfirm
-sudo pacman -S wlroots --needed --noconfirm
-sudo pacman -S xorg-xwayland --nedeed --noconfirm
+sudo aura -S stow --needed --noconfirm
+sudo aura -S wlroots --needed --noconfirm
+sudo aura -S xorg-xwayland --nedeed --noconfirm
 sudo aura -Aca river-git --needed
 sudo aura -Aca rofi-lbonn-wayland-git --needed
 sudo aura -Aca swaybg-git --needed
 sudo aura -Aca waybar-git --needed
+sudo aura -Aca wlrobs-hg --neded
+sudo aura -S pipewire --needed
+sudo aura -S obs-studio
 sudo aura -Oj
+
 mv ~/.config/rofi ~/.config/rofi-backup
 mv ~/.config/river ~/.config/river-backup
 mv ~/.config/waybar ~/.config/waybar-backup
