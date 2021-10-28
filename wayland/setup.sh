cf7a150fc0397759dc778d44c7fafca2925202c4
Shinyzenith <aakashsensharma@gmail.com>
2021-10-28 10:40:07 +0530

    script and river config update

--- wayland/setup.sh
+++ wayland/setup.sh
@@ -1,18 +1,31 @@
-sudo aura -S stow --needed --noconfirm
-sudo aura -S wlroots --needed --noconfirm
-sudo aura -S xorg-xwayland --nedeed --noconfirm
-sudo aura -Aca river-git --needed
-sudo aura -Aca rofi-lbonn-wayland-git --needed
-sudo aura -Aca swaybg-git --needed
-sudo aura -Aca waybar-git --needed
+#!/usr/bin/env bash
+
+# window manager minimum needs
+sudo aura -S --needed --noconfirm stow
+sudo aura -S --needed --noconfirm wlroots
+sudo aura -S --needed --noconfirm xorg-xwayland
+sudo aura -Aca --needed --noconfirm river-git
+sudo aura -Aca --needed --noconfirm rofi-lbonn-wayland-git
+sudo aura -Aca --needed --noconfirm swaybg-git
+sudo aura -Aca --needed --noconfirm waybar-git
+
+# OBS packages
 sudo aura -Aca wlrobs-hg --neded
 sudo aura -S pipewire --needed
 sudo aura -S obs-studio
+
+# screenshot utilities
+sudo aura -S grim --needed --noconfirm
+sudo aura -S slurp --needed --noconfirm
+sudo aura -S wl-clipboard --needed --noconfirm
+
+#cleaning up orphans
 sudo aura -Oj
 
 mv ~/.config/rofi ~/.config/rofi-backup
 mv ~/.config/river ~/.config/river-backup
 mv ~/.config/waybar ~/.config/waybar-backup
+cd ~/.dotfiles/wayland
 stow river -t ~/.config/
 stow waybar -t ~/.config/
 stow rofi -t ~/.config/
